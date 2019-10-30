29abd78b3a0376272c0806d09803bafba4966f05
RafaelMiranda21 <38355955+RafaelMiranda21@users.noreply.github.com>
2019-10-30 15:32:15 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+
+<form method="GET" action="calculo.php">
+  <input type="number" name="n1">
+  <select name="op">
+    <option value="1">+</option>
+    <option value="2">-</option>
+    <option value="3">/</option>
+    <option value="4">*</option>
+  </select>
+  <input type="number" name="n2">
+  <input type="submit" value="Calcular">
+</form>
